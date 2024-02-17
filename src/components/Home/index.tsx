8c6a8908493ac5f613c90e37cdaf4ac21db6f841
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2024-02-17 17:33:02 +0900

    feat : topnavigation

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+import TopNavigation from "../common/TopNavigation";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [section, setSection] = useState(0);
+
+  const nextPage = () => {
+    setSection(1);
+  };
+
+  const prevPage = () => {
+    setSection(0);
+  };
+  const onSubmit = () => {
+    console.log("제출 하기 함수");
+  };
+  return (
+    <TopNavigation
+      section={section}
+      nextPage={nextPage}
+      prevPage={prevPage}
+      onSubmit={onSubmit}
+    />
+  );
 };
 
 export default Home;
--- src/components/common/TopNavigation/index.tsx
+++ src/components/common/TopNavigation/index.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { Container, Text } from "./style";
+import LeftArrowIcon from "../../../assets/LeftArrowIcon";
 
-const TopNavigation = () => {
-  return <div>TopNavigation</div>;
+interface IFTopNavigationProps {
+  section: number;
+  prevPage: () => void;
+  nextPage: () => void;
+  onSubmit: () => void;
+  children?: string;
+}
+
+const TopNavigation = ({
+  nextPage,
+  prevPage,
+  children,
+  section,
+  onSubmit,
+}: IFTopNavigationProps) => {
+  return (
+    <Container>
+      <LeftArrowIcon onClick={prevPage} />
+      {children}
+      <Text onClick={section === 0 ? nextPage : onSubmit} section={section}>
+        {section === 0 ? "다음으로" : "게시하기"}
+      </Text>
+    </Container>
+  );
 };
 
 export default TopNavigation;
