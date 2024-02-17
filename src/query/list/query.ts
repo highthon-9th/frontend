9ea76f71233f888e1ab8b4efac42d6a3c3a08548
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2024-02-18 04:25:44 +0900

    좋아요 좋아요 취소 api 작성

--- src/query/list/query.ts
+++ src/query/list/query.ts
@@ -1,5 +1,15 @@
-import { useQuery } from "react-query";
-import { getList } from "../../repository/list";
+import { useMutation, useQuery } from "react-query";
+import { getList, likeList, unLikeList } from "../../repository/list";
 
 export const useGetListQuery = (date: number) =>
   useQuery(["post/getPost", date], () => getList(date));
+
+export const useLikeListMutation = () => {
+  const mutation = useMutation(likeList);
+  return mutation;
+};
+
+export const useUnLikeListMutation = () => {
+  const mutation = useMutation(unLikeList);
+  return mutation;
+};
--- src/repository/list/index.ts
+++ src/repository/list/index.ts
@@ -5,3 +5,11 @@ export const getList = async (date: number) => {
   const { data } = await instance.get<Post[]>(`/post?generation=${date}`);
   return data;
 };
+
+export const likeList = async (id: number) => {
+  await instance.post(`/post/${id}/like`);
+};
+
+export const unLikeList = async (id: number) => {
+  await instance.delete(`/post/${id}/like`);
+};
