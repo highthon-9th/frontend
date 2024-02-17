af2cf2cdce4f64cf0918d3f157128e73c5e444f1
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2024-02-18 02:54:17 +0900

    toast 추가

--- src/components/BBiBBi/index.tsx
+++ src/components/BBiBBi/index.tsx
@@ -2,12 +2,13 @@ import { styled } from "styled-components";
 import BottomNavigation from "../common/BottomNavigation";
 import AddButton from "../common/AddButton";
 import { useNavigate } from "react-router-dom";
+import Toast from "../common/Toast";
 
 const BBiBBi = () => {
   const navigate = useNavigate();
   return (
     <Container>
-      BBIBBI
+      <Toast />
       <AddButton navigate={() => navigate("/bbibbiform")} />
       <BottomNavigation />
     </Container>
